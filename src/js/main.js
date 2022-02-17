63df86d3c27eb054890ffc689e73cde480ab8a28
Fanbayy <fandi.bayu110@gmail.com>
2022-02-18 01:58:40 +0700

    added home page fitur and completed detail

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".swiper", {
+  // Optional parameters
+  freeMode: 3,
+  loop: false,
+  speed: 400,
+  grabCursor: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
