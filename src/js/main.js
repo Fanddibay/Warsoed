f4be6ef8e8f34ede72186dce71216b9286714d30
Fanbayy <fandi.bayu110@gmail.com>
2022-03-04 00:29:25 +0700

    added admin pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".swiper", {
+let swiper = new Swiper(".swiper", {
   // Optional parameters
 
   loop: false,
@@ -52,3 +52,64 @@ function activeWork() {
 }
 
 linkWork.forEach((l) => l.addEventListener("click", activeWork));
+
+// airplane js
+function data() {
+  function getThemeFromLocalStorage() {
+    // if user already changed the theme, use it
+    if (window.localStorage.getItem("dark")) {
+      return JSON.parse(window.localStorage.getItem("dark"));
+    }
+
+    // else return their preferences
+    return !!window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
+  function setThemeToLocalStorage(value) {
+    window.localStorage.setItem("dark", value);
+  }
+
+  return {
+    dark: getThemeFromLocalStorage(),
+    toggleTheme() {
+      this.dark = !this.dark;
+      setThemeToLocalStorage(this.dark);
+    },
+    isSideMenuOpen: false,
+    toggleSideMenu() {
+      this.isSideMenuOpen = !this.isSideMenuOpen;
+    },
+    closeSideMenu() {
+      this.isSideMenuOpen = false;
+    },
+    isNotificationsMenuOpen: false,
+    toggleNotificationsMenu() {
+      this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen;
+    },
+    closeNotificationsMenu() {
+      this.isNotificationsMenuOpen = false;
+    },
+    isProfileMenuOpen: false,
+    toggleProfileMenu() {
+      this.isProfileMenuOpen = !this.isProfileMenuOpen;
+    },
+    closeProfileMenu() {
+      this.isProfileMenuOpen = false;
+    },
+    isPagesMenuOpen: false,
+    togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen;
+    },
+    // Modal
+    isModalOpen: false,
+    trapCleanup: null,
+    openModal() {
+      this.isModalOpen = true;
+      this.trapCleanup = focusTrap(document.querySelector("#modal"));
+    },
+    closeModal() {
+      this.isModalOpen = false;
+      this.trapCleanup();
+    },
+  };
+}
